8c2cb39efe114c82cafa60b0d95fe508c7fa0017
Noah Skelton <skelton.noah@gmail.com>
2020-10-04 10:55:53 +0100

    Tests updated to include code coverage

--- app.py
+++ app.py
@@ -7,37 +7,33 @@ data = {}
 
 @app.route('/todo', methods=['GET'])
 def get_todos():
-    if request.method == 'GET':
-        if request.form:
-            obj_id = request.form["obj_id"]
-            return jsonify(data[obj_id])
-        else:
-            return jsonify(data)
+    if request.form:
+        obj_id = request.form["obj_id"]
+        return jsonify(data[obj_id])
+    else:
+        return jsonify(data)
 
 
 @app.route('/todo', methods=['POST'])
 def create_todo():
-    if request.method == 'POST':
-        payload = request.form
-        obj_id = payload["obj_id"]
-        data[obj_id] = payload["text"]
+    payload = request.form
+    obj_id = payload["obj_id"]
+    data[obj_id] = payload["text"]
 
 
 @app.route('/todo', methods=['PATCH'])
 def edit_todo():
-    if request.method == 'PATCH':
-        payload = request.form
-        obj_id = payload["obj_id"]
-        data[obj_id] = payload["text"]
+    payload = request.form
+    obj_id = payload["obj_id"]
+    data[obj_id] = payload["text"]
 
 
 @app.route('/todo', methods=['DELETE'])
 def delete_todo():
-    if request.method == 'DELETE':
-        payload = request.form
-        obj_id = payload["obj_id"]
-        del data[obj_id]
+    payload = request.form
+    obj_id = payload["obj_id"]
+    del data[obj_id]
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # pragma: no cover
     app.run()
