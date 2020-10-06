2ec3d94e72d186deb3237fca9f11f53e5986e9e4
Noah Skelton <skelton.noah@gmail.com>
2020-10-06 09:36:36 +0100

    Fixed flake8 for CI

--- src/todo_app/app.py
+++ src/todo_app/app.py
@@ -1,16 +1,14 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, Response, request
+import uuid
 import json
 from typing import Dict
-import uuid
 
 app = Flask(__name__)
-import json
-from flask.testing import FlaskClient
-from typing import List, Dict, Generator
+
 data: Dict[str, str] = {}
 
 
-@app.route('/todo', methods=['GET'])
+@app.route("/todo", methods=["GET"])
 def get_todos() -> Response:
 
     return Response(
@@ -18,57 +16,51 @@ def get_todos() -> Response:
         mimetype="application/json",
     )
 
-@app.route('/todo/<string:obj_id>', methods=['GET'])
+
+@app.route("/todo/<string:obj_id>", methods=["GET"])
 def get_todo(obj_id: str) -> Response:
     if obj_id not in data:
-        return Response(
-            status=404
-        )
+        return Response(status=404)
     return Response(
         response=json.dumps(data[obj_id]),
         mimetype="application/json",
     )
 
 
-@app.route('/todo', methods=['POST'])
+@app.route("/todo", methods=["POST"])
 def create_todo() -> Response:
     payload = request.json
     obj_id = uuid.uuid4().hex
     data[obj_id] = payload["text"]
 
     return Response(
-        response=json.dumps({"obj_id":obj_id}),
+        response=json.dumps({"obj_id": obj_id}),
         mimetype="application/json",
-        status=201
+        status=201,
     )
 
 
-@app.route('/todo/<string:obj_id>', methods=['PATCH'])
+@app.route("/todo/<string:obj_id>", methods=["PATCH"])
 def edit_todo(obj_id: str) -> Response:
 
     if obj_id not in data:
-        return Response(
-            status=404
-        )
+        return Response(status=404)
 
     payload = request.json
     data[obj_id] = payload["text"]
 
-
     return Response()
 
 
-@app.route('/todo/<string:obj_id>', methods=['DELETE'])
+@app.route("/todo/<string:obj_id>", methods=["DELETE"])
 def delete_todo(obj_id: str) -> Response:
 
     if obj_id not in data:
-        return Response(
-            status=404
-        )
+        return Response(status=404)
     del data[obj_id]
 
     return Response()
 
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     app.run()
--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,5 +1,3 @@
-from flask import Flask
-
 from flask.testing import FlaskClient
 from typing import Dict
 from todo_app.app import data
@@ -7,77 +5,81 @@ from todo_app.app import data
 
 class TestCreate:
     def test_create(self, client: FlaskClient) -> None:
-        post_res = client.post('/todo', json={"text": "test_text"})
+        post_res = client.post("/todo", json={"text": "test_text"})
         assert post_res.status_code == 201
 
+
 class TestGetAllTodos:
-
     def test_empty(self, client: FlaskClient) -> None:
-        res = client.get('/todo')
+        res = client.get("/todo")
 
-        get_data = res.get_json()
+        res.get_json()
         assert res.status_code == 200
         expected_data: Dict = {}
         assert data == expected_data
 
-
     def test_get_all(self, client: FlaskClient) -> None:
-        first_post = client.post('/todo', json={"text": "test_text"})
-        second_post = client.post('/todo', json={"text": "test_text_2"})
-        first_obj_id = first_post.get_json()['obj_id']
-        second_obj_id = second_post.get_json()['obj_id']
-        res = client.get('/todo')
+        first_post = client.post("/todo", json={"text": "test_text"})
+        second_post = client.post("/todo", json={"text": "test_text_2"})
+        first_obj_id = first_post.get_json()["obj_id"]
+        second_obj_id = second_post.get_json()["obj_id"]
+        res = client.get("/todo")
         get_data = res.get_json()
         assert res.status_code == 200
         expected_data: Dict = {
-            first_obj_id: 'test_text',
-            second_obj_id: 'test_text_2',
+            first_obj_id: "test_text",
+            second_obj_id: "test_text_2",
         }
         assert get_data == expected_data
 
+
 class TestEdit:
     def test_exists(self, client: FlaskClient) -> None:
-        result_from_post = client.post('/todo', json={"text": "test_text"})
-        obj_id = result_from_post.get_json()['obj_id']
-        patch_result = client.patch('/todo/' + obj_id, json={"text": "test_text_edited"})
+        result_from_post = client.post("/todo", json={"text": "test_text"})
+        obj_id = result_from_post.get_json()["obj_id"]
+        patch_result = client.patch(
+            "/todo/" + obj_id, json={"text": "test_text_edited"}
+        )
         assert patch_result.status_code == 200
-        res = client.get('/todo/' + obj_id)
+        res = client.get("/todo/" + obj_id)
         get_data = res.get_json()
         expected_data = "test_text_edited"
         assert get_data == expected_data
 
     def test_does_not_exist(self, client: FlaskClient) -> None:
         data.clear()
-        patch_result = client.patch('/todo/1', json={"text": "test_text_edited"})
+        patch_result = client.patch(
+            "/todo/1",
+            json={"text": "test_text_edited"},
+        )
         assert patch_result.status_code == 404
 
-class TestGetOneTodo:
 
+class TestGetOneTodo:
     def test_exists(self, client: FlaskClient) -> None:
-        result_from_post = client.post('/todo', json={"text": "test_text"})
-        obj_id = result_from_post.get_json()['obj_id']
-        res = client.get('/todo/' + obj_id)
+        result_from_post = client.post("/todo", json={"text": "test_text"})
+        obj_id = result_from_post.get_json()["obj_id"]
+        res = client.get("/todo/" + obj_id)
         get_data = res.get_json()
         assert res.status_code == 200
-        expected_data = 'test_text'
+        expected_data = "test_text"
         assert get_data == expected_data
 
     def test_does_not_exist(self, client: FlaskClient) -> None:
-        res = client.get('/todo/1')
+        res = client.get("/todo/1")
         assert res.status_code == 404
 
 
 class TestDelete:
-
     def test_exists(self, client: FlaskClient) -> None:
-        result_from_post = client.post('/todo', json={"text": "test_text"})
-        obj_id = result_from_post.get_json()['obj_id']
-        delete_res = client.delete('/todo/' + obj_id)
-        get_data = client.get('/todo/' + obj_id)
+        result_from_post = client.post("/todo", json={"text": "test_text"})
+        obj_id = result_from_post.get_json()["obj_id"]
+        delete_res = client.delete("/todo/" + obj_id)
+        get_data = client.get("/todo/" + obj_id)
         assert delete_res.status_code == 200
-        get_data = client.get('/todo/' + obj_id)
+        get_data = client.get("/todo/" + obj_id)
         assert get_data.status_code == 404
 
     def test_does_not_exist(self, client: FlaskClient) -> None:
-        delete_res = client.delete('/todo/1')
+        delete_res = client.delete("/todo/1")
         assert delete_res.status_code == 404
