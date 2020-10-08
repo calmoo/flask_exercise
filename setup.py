7b570c203c8ea5de5fe90dd07e2358d20876a42f
Noah Skelton <skelton.noah@gmail.com>
2020-10-08 12:42:31 +0100

    Fixed syntax terror in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="todo_app",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=["Flask==1.1.2", "SQLAlchemy=1.3.19"],
+    install_requires=["Flask==1.1.2", "SQLAlchemy==1.3.19"],
 )
